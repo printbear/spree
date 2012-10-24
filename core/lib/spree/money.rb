245674b3babd2beea574340a47a89b4e01484071
Gregor MacDougall <gregor@freerunningtechnologies.com>
2012-10-24 11:39:17 -0700

    Add currency parameter support to Spree::Money.
    Spree::Money now allows an optional currency parameter to be passed in,
    which will be used if specified.  In the case where it is not specified,
    the global spree configuration value will be used instead.

--- core/lib/spree/money.rb
+++ core/lib/spree/money.rb
@@ -3,7 +3,7 @@ require 'money'
 module Spree
   class Money
     def initialize(amount, options={})
-      @money = ::Money.parse([amount, Spree::Config[:currency]].join)
+      @money = ::Money.parse([amount, (options[:currency] || Spree::Config[:currency])].join)
       @options = {}
       @options[:with_currency] = true if Spree::Config[:display_currency]
       @options[:symbol_position] = Spree::Config[:currency_symbol_position].to_sym
