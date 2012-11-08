d923a6f79ec728c01a57031f6763f149e22bcbdd
Gregor MacDougall <gregor@freerunningtechnologies.com>
2012-11-08 15:30:35 -0800

    Fixed same shipping EUR.

--- sample/db/sample/spree/preferences.rb
+++ sample/db/sample/spree/preferences.rb
@@ -4,7 +4,7 @@ shipping_method.calculator.preferred_currency = 'USD'
 
 shipping_method = Spree::ShippingMethod.find_by_name("UPS Ground (EUR)")
 shipping_method.calculator.preferred_amount = 5
-shipping_method.calculator.preferred_currency = 'USD'
+shipping_method.calculator.preferred_currency = 'EUR'
 
 shipping_method = Spree::ShippingMethod.find_by_name("UPS One Day (USD)")
 shipping_method.calculator.preferred_amount = 15
