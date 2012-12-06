solidusio/solidus

Fix incompatibility with squeel gem
Fixes spree/spree#2261

core/app/models/spree/preferences/preferable_class_methods.rb
@@ -15,7 +15,7 @@ module Spree::Preferences
         else
           if get_pending_preference(name)
             get_pending_preference(name)
-          elsif Spree::Preference.table_exists? && preference = Spree::Preference.find_by_key(name)
+          elsif Spree::Preference.table_exists? && preference = Spree::Preference.find_by_key(name.to_s)
             preference.value
           else
             send self.class.preference_default_getter_method(name)

printbear/spree

Backport of #2261 fix to 1-1-stable.

core/app/models/spree/preferences/preferable_class_methods.rb
@@ -15,7 +15,7 @@ module Spree::Preferences
         else
           if get_pending_preference(name)
             get_pending_preference(name)
-          elsif Spree::Preference.table_exists? && preference = Spree::Preference.find_by_name(name)
+          elsif Spree::Preference.table_exists? && preference = Spree::Preference.find_by_name(name.to_s)
             preference.value
           else
             send self.class.preference_default_getter_method(name)