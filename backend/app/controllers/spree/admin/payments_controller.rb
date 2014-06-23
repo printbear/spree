b750acd313b23cf321eb790d55a4a88e9efe3906
John Hawthorn <john@freerunningtechnologies.com>
2014-06-23 11:46:20 -0700

    PaymentsController: load_order for all actions

--- backend/app/controllers/spree/admin/payments_controller.rb
+++ backend/app/controllers/spree/admin/payments_controller.rb
@@ -1,7 +1,7 @@
 module Spree
   module Admin
     class PaymentsController < Spree::Admin::BaseController
-      before_filter :load_order, :only => [:create, :new, :index, :fire]
+      before_filter :load_order
       before_filter :load_payment, :except => [:create, :new, :index]
       before_filter :load_data
       before_filter :can_transition_to_payment
