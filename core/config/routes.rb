a9f6bbb2f5964dffbf4aa38e46e7f8f771f44328
Clarke Brunsdon <clarke@freerunningtechnologies.com>
2014-08-21 15:38:16 -0700

    Okay. Hack to allow this file to be run multiple times
    terrible hack etc. nobody judge.

--- core/config/routes.rb
+++ core/config/routes.rb
@@ -2,6 +2,9 @@ Spree::Core::Engine.draw_routes
 
 # yes, terrible, terrible hack. but it guarantees that no extension
 # has appended to spree's routes that we are clobbering
-Spree::Core::Engine.routes.append do
-  get '/*id', :to => 'taxons#show', :as => :nested_taxons
+if !defined?(ROUTEHACK)
+  Spree::Core::Engine.routes.append do
+    get '/*id', :to => 'taxons#show', :as => :nested_taxons
+  end
+  class ROUTEHACK; end;
 end
