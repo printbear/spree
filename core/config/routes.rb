991f2b0bf373181ac02a3e912544bd64aacf52e8
Clarke Brunsdon <clarke@freerunningtechnologies.com>
2014-08-21 14:55:54 -0700

    Hack taxon glob after all spree routes
    This is a terrible hack and right here since our glob will clobber any
    potential routes added by extensions via append_routes

--- core/config/routes.rb
+++ core/config/routes.rb
@@ -1,1 +1,7 @@
 Spree::Core::Engine.draw_routes
+
+# yes, terrible, terrible hack. but it guarantees that no extension
+# has appended to spree's routes that we are clobbering
+Spree::Core::Engine.routes.append do
+  get '/*id', :to => 'taxons#show', :as => :nested_taxons
+end
