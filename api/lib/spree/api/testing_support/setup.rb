83d6a28c6029eaddb6a8bbfd3713dc1cfb0aacc3
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-10-24 13:45:54 -0400

    Fix remaining deprecations in helpers.
    (cherry picked from commit 0751d448a807cd8ef9df2bd70c211d79f9b0ebc4 in spree/spree)
    
    Conflicts:
    	api/lib/spree/api/testing_support/setup.rb

--- api/lib/spree/api/testing_support/setup.rb
+++ api/lib/spree/api/testing_support/setup.rb
@@ -5,7 +5,7 @@ module Spree
         def sign_in_as_admin!
           let!(:current_api_user) do
             user = stub_model(Spree::LegacyUser)
-            user.stub(:has_spree_role?).with("admin").and_return(true)
+            allow(user).to receive(:has_spree_role?).with("admin").and_return(true)
             user
           end
         end
