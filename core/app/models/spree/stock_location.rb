3ba7876b6d9807d4ba2538e41844ab78cbee9404
Jordan Brough <git.j@brgh.net>
2015-03-06 11:23:53 -0700

    Add Carton table & model

--- core/app/models/spree/stock_location.rb
+++ core/app/models/spree/stock_location.rb
@@ -1,6 +1,7 @@
 module Spree
   class StockLocation < ActiveRecord::Base
     has_many :shipments
+    has_many :cartons, inverse_of: :stock_location
     has_many :stock_items, dependent: :delete_all
     has_many :stock_movements, through: :stock_items
 
---
+++ core/db/migrate/20150305043021_create_spree_cartons.rb
@@ -0,0 +1,20 @@
+class CreateSpreeCartons < ActiveRecord::Migration
+  def change
+    create_table "spree_cartons" do |t|
+      t.string "number", index: true
+
+      t.references "order", index: true
+      t.references "stock_location", index: true
+      t.references "address"
+      t.references "shipping_method"
+
+      t.string "tracking"
+
+      t.datetime "shipped_at"
+
+      t.timestamps
+    end
+
+    add_column "spree_inventory_units", "carton_id", :integer, index: true
+  end
+end
