0335d9506336948ad1ee34d20cd8892a9ba61ee7
Jordan Brough <git.j@brgh.net>
2015-03-06 11:23:53 -0700

    Add Carton table & model
    This is the first of several steps to extract the concept of a "shipped"
    shipment *out* of the Shipment model into a separate model.
    
    A "Carton" is meant to represent a collection of inventory units that have
    actually been shipped. A Carton must have inventory units and may have an
    optional external_number (e.g. a 3PL identifier) in addition to a tracking
    number.
    
    Note: The name "Carton" is a bit confusing since it represents and actual
    *shipment* whereas "Shipment" will now just represent a "pending shipment" or
    "billed shipment" or something like that. However, it's not practical at the moment to rename the "Shipment"
    model and table.  Also we're hopeful that eventually the Shipment model itself
    will be further broken down and possibly go away entirely.

--- core/app/models/spree/inventory_unit.rb
+++ core/app/models/spree/inventory_unit.rb
@@ -3,6 +3,7 @@ module Spree
     belongs_to :variant, class_name: "Spree::Variant", inverse_of: :inventory_units
     belongs_to :order, class_name: "Spree::Order", inverse_of: :inventory_units
     belongs_to :shipment, class_name: "Spree::Shipment", touch: true, inverse_of: :inventory_units
+    belongs_to :carton, class_name: "Spree::Carton", inverse_of: :inventory_units
     belongs_to :return_authorization, class_name: "Spree::ReturnAuthorization"
     belongs_to :line_item, class_name: "Spree::LineItem", inverse_of: :inventory_units
 
