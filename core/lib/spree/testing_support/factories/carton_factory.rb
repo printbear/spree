f244f9e68aeb3bf098db1065b12214efd2729490
Jordan Brough <git.j@brgh.net>
2015-03-12 15:25:46 -0600

    update carton factory to eliminate extraneous stock locations

--- core/lib/spree/testing_support/factories/carton_factory.rb
+++ core/lib/spree/testing_support/factories/carton_factory.rb
@@ -3,7 +3,15 @@ FactoryGirl.define do
     address
     stock_location
     shipping_method
-    inventory_units { [create(:inventory_unit)] }
     shipped_at { Time.now }
+    inventory_units do
+      [
+        build(
+          :inventory_unit,
+          # ensure the shipment uses the same stock location as the carton
+          shipment: build(:shipment, stock_location: stock_location)
+        ),
+      ]
+    end
   end
 end
