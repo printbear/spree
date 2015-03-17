4e827b0ba16481cdec5bbc50ddef3abee259f708
Phillip Birtcher <pbirtcher@bonobos.com>
2015-03-17 10:57:30 -0400

    Modify params documentation on promo code builder

--- core/app/models/spree/promotion_code_builder.rb
+++ core/app/models/spree/promotion_code_builder.rb
@@ -4,6 +4,11 @@ class Spree::PromotionCodeBuilder
   class_attribute :default_random_code_length
   self.default_random_code_length = 6
 
+  # @param promotion_attrs [Hash] The desired attributes for the newly promotion
+  # @param base_code [String] When number_of_codes=1 this is the code. When
+  #   number_of_codes > 1 it is the base of the generated codes.
+  # @param number_of_codes [Integer] Number of codes to generate
+  # @param user [Spree::User] The user who triggered this promotion build
   def initialize(promotion_attrs:, base_code:, number_of_codes:, user: nil)
     @promotion_attrs = promotion_attrs
     @promotion = Spree::Promotion.new(@promotion_attrs)
@@ -17,13 +22,8 @@ class Spree::PromotionCodeBuilder
     @promotion.save
   end
 
-  # Build promo codes. If number_of_codes is great than one then generate
+  # Build promo codes. If @number_of_codes is greater than one then generate
   # multiple codes by adding a random suffix to each code.
-  #
-  # @param base_code [String] When number_of_codes=1 this is the code. When
-  #   number_of_codes > 1 it is the base of the generated codes.
-  # @param number_of_codes [Integer] Number of codes to generate
-  # @param usage_limit [Integer] Usage limit for each code
   def build_promotion_codes
     if @number_of_codes == 1
       @promotion.codes.build(value: @base_code)
