f3f5516cd587eaafc547d9be8ea562b02bbf302a
Lucas Húngaro <lucashungaro@gmail.com>
2016-07-05 11:58:09 -0300

    Add ShipmentsController#update back to API (used on admin)

--- api/app/controllers/spree/api/shipments_controller.rb
+++ api/app/controllers/spree/api/shipments_controller.rb
@@ -5,6 +5,26 @@ module Spree
 
       before_filter :find_order
 
+      def update
+        authorize! :read, Shipment
+        @shipment = @order.shipments.find_by_number!(params[:id])
+        params[:shipment] ||= []
+        unlock = params[:shipment].delete(:unlock)
+
+        if unlock == 'yes'
+          @shipment.adjustment.open
+        end
+
+        @shipment.update_attributes(params[:shipment])
+
+        if unlock == 'yes'
+          @shipment.adjustment.close
+        end
+
+        @shipment.reload
+        respond_with(@shipment, :default_template => :show)
+      end
+
       private
 
       def find_order
